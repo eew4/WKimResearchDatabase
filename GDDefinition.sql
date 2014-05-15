e0468eeb9fdc3407a83d4f7e29f94450cb61afda
eew4 <eew4@uw.edu>
2014-05-14 22:12:23 -0700

    First Sincere DDL Commit

--- GDDefinition.sql
+++ GDDefinition.sql
@@ -1,101 +1,89 @@
 
 CREATE DATABASE dbSequenced;
 
+--Primary Tables
 CREATE TABLE tblReferenceSequences{
-	SEQID		varchar(50) CONSTRAINT pk_tblReferenceSequences PRIMARY KEY
-}
-
-CREATE TABLE tblSNPMutation{
-	SNPID		integer CONSTRAINT pk_tblSNPMutation PRIMARY KEY,
-	SEQID		varchar(50),
-	position	integer,
-	newSeq		varchar(20)
+	RefSequenceID		integer CONSTRAINT pk_tblReferenceSequences PRIMARY KEY,
+	ReferenceSequence 	varchar(MAX)
 };
 
-CREATE TABLE tblSUBMutation{
-	SUBID		integer CONSTRAINT pk_tblSUBMutation PRIMARY KEY,
-	SEQID		varchar(50),
-	position	integer,
-	size		integer,
-	newSeq		varchar(20)
+CREATE TABLE tblSequences{
+	SequenceID			integer CONSTRAINT pk_tblSequences PRIMARY KEY,
+	RefSequenceID 		integer REFERENCES tblReferenceSequences (RefSequenceID),
+	RefDate				timestamp
 };
 
-CREATE TABLE tblDELMutation{
-	DELID		integer CONSTRAINT pk_tblDELMutation PRIMARY KEY,
-	SEQID		varchar(50),
-	position	integer,
-	size		integer
+CREATE TABLE tblFastqs{
+	FastqID				integer CONSTRAINT pk_tblFastqs PRIMARY KEY,
+	FastqFileName		varchar(50)
 };
 
-CREATE TABLE tblINSMutation{
-	INSID		integer CONSTRAINT pk_tblINSMutation PRIMARY KEY,
-	SEQID 		varchar(50),
-	position 	integer,
-	newSeq 		varchar(20)
+CREATE TABLE tblMutations{
+	MutationID			integer CONSTRAINT pk_tblMutations PRIMARY KEY,
+	MutationType		char(3) NOT NULL,
+	Position			integer,
+	Size				integer,
+	NewSequence			varchar(50),
+	RepeatName			varchar(50),
+	Strand				boolean,
+	DuplicationSize		integer,
+	NewCopyNumber		integer,
+	--double check for data type on Region
+	Region				varchar(MAX)
+	--key value attributes
 };
-
-CREATE TABLE tblMOBMutation{
-	MOBID		integer CONSTRAINT pk_tblMOBMutation PRIMARY KEY,
-	SEQID 		varchar(50),
-	position 	integer,
-	repeatName	varchar(50),
-	strand		boolean,
-	duplicationSize	integer
-}
-
-CREATE TABLE tblAMPMutation{
-	AMPID		integer CONSTRAINT pk_tblAMPMutation PRIMARY KEY
-}
-
-CREATE TABLE tblCONMutation{
-	CONID		integer CONSTRAINT pk_tblCONMutation PRIMARY KEY
-}
-
-CREATE TABLE tblINVMutation{
-	INVID		integer CONSTRAINT pk_tblINVMutation PRIMARY KEY
-}
 
 CREATE TABLE tblRAEvidence{
-	RAID 		integer CONSTRAINT pk_tblRAEvidence PRIMARY KEY
-}
+	RAEvidenceID		integer CONSTRAINT pk_tblRAEvidence PRIMARY KEY,
+	Position 			integer,
+	InsertPosition 		integer,
+	RefBase 			char(1),
+	NewBase 			char(1)
+	-- Key Valued pairs
+};
 
 CREATE TABLE tblMCEvidence{
-	MCID 		integer CONSTRAINT pk_tblMCEvidence	PRIMARY KEY
-}
+	MCEvidenceID 		integer CONSTRAINT pk_tblMCEvidence PRIMARY KEY,
+	Start				integer,
+	End					integer,
+	StartRange			integer,
+	EndRange			integer
+	--Key Valued Pairs
+};
 
 CREATE TABLE tblJCEvidence{
-	JCID 		integer CONSTRAINT pk_tblJCEvidence PRIMARY KEY
-}
+	JCEvidenceID		integer CONSTRAINT pk_tblJCEvidence PRIMARY KEY,
+
+};
 
 CREATE TABLE tblUNEvidence{
-	UNID 		integer CONSTRAINT pk_tblUNEvidence PRIMARY KEY
-}
+	UNEvidenceID 		integer CONSTRAINT pk_tblUNEvidence PRIMARY KEY,
+	Start 				integer,
+	End 				integer
+};
 
-CREATE TABLE tblTSEQValidation{
-	TSEQID 		integer CONSTRAINT pk_tblTSEQValidation PRIMARY KEY
-}
+CREATE TABLE tblValidations{
+	ValidationID		integer CONSTRAINT pk_tblValidations PRIMARY KEY,
+	ValidationType		char(4)
+};
 
-CREATE TABLE tblPFLPValidation{
-	PFLPID 		integer CONSTRAINT pk_tblPFLPValidation PRIMARY KEY
-}
-
-CREATE TABLE tblRFLPValidation{
-	RFLPID 		integer CONSTRAINT pk_tblRFLPValidation PRIMARY KEY
-}
-
-CREATE TABLE tblPFGEValidation{
-	PFGEID 		integer CONSTRAINT pk_tblPFGEValidation PRIMARY KEY
-}
-
-CREATE TABLE tblPHYLValidation{
-	PHYLID 		integer CONSTRAINT pk_tblPHYLValidation PRIMARY KEY
-}
-
-CREATE TABLE tblCURAValidation{
-	CURAID 		integer CONSTRAINT pk_tblCURAValidation PRIMARY KEY
-}
-
+--Bridge Tables
 CREATE TABLE tblMutationEvidence{
-	MutationEvidenceID	integer CONSTRAINT pk_tblMutationEvidence PRIMARY KEY,
-	SNPID				integer
+	MutationID 			integer REFERENCES tblMutations (MutationID),
+	EvidenceID 			integer REFERENCES tblEvidence (EvidenceID)
+};
+
+CREATE TABLE tblMutationValidation{
+	MutationID 			integer REFERENCES tblMutations (MutationID),
+	ValidationID		integer REFERENCES tblValidations (ValidationID)
+}
+
+CREATE TABLE tblMutationFastqs{
+	FastqID 			integer REFERENCES tblFastqs (FastqID),
+	MutationID 			integer REFERENCES tblMutations (MutationID)
+};
+
+CREATE TABLE tblFastqEvidence{
+	FastqID 			integer REFERENCES tblFastqs (FastqID),
+	EvidenceID 			integer REFERENCES tblEvidence (EvidenceID)
 }
